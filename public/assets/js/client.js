a3bb9d094e51ab55166bde4270ae3ae4cf171a39
Scott Daniels <sdaniels6363@gmail.com>
2019-11-20 18:33:09 -0500

    base client js for scraper-btn

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,6 +1,10 @@
 $(document).ready(() => {
-
-  
-
-
+  $("#scraper-btn").on("click", () => {
+    $.ajax({
+      method: "GET",
+      url: "/scrape"
+    }).then(response => {
+      console.log("Scraping started.");
+    });
+  });
 });
