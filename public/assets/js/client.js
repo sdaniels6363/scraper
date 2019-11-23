fc027cb7934575f3a8d88985aeaab6b2170f40c9
Scott Daniels <sdaniels6363@gmail.com>
2019-11-22 22:10:56 -0500

    comment retrieve and submit call not working 100%

--- public/assets/js/client.js
+++ public/assets/js/client.js
@@ -1,11 +1,52 @@
 $(document).ready(() => {
+  
   $("#scraper-btn").on("click", () => {
     $.ajax({
       method: "GET",
       url: "/scrape"
     }).then(response => {
       console.log("Scraping complete.");
+      console.log(response)
       location.reload();
     });
   });
+
+  $(".comments").on("click",(event)=>{
+
+    console.log("Comments requested.")
+    let mediaId = event.currentTarget.dataset.id
+    $.ajax({
+      method: "POST",
+      url: "/comments/retrieve",
+      data:{
+        id: mediaId
+      }
+    }).then(response => {
+      console.log("Comments delivered.")
+    });
+  })
+
+  $("#submitComment").on("click", (event)=> {
+    event.preventDefault()
+
+    let name = $("#commenterName").val();
+    let comment = $("#commenterComment").val();
+
+    $.ajax({
+      method: "POST",
+      url: "/comments/submit",
+      data:{
+        name: name,
+        comment: comment
+      } 
+    }).then(response => {
+      console.log(response)
+
+
+
+    });
+
+  });
+
+
 });
