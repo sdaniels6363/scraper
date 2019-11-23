509c644d7372c0a059b43088bf065aa91eb8bc18
Scott Daniels <sdaniels6363@gmail.com>
2019-11-22 22:09:27 -0500

    added some options to mongoose connection

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("view engine", "handlebars");
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
-mongoose.connect(MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true } );
 
 
 // Import routes and give the server access to them.
